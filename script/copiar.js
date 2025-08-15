d12bfac6486da70c6dcdd529936ce6d71b305e4c
Maria Yasmin <mariayasmim.wtf@gmail.com>
2025-08-15 17:01:03 -0300

    changes

--- script/copiar.js
+++ script/copiar.js
@@ -1,16 +1,15 @@
 
 const btnCopiar = document.getElementById("btnCopiar");
 // Texto que queremos copiar para a área de transferência
-const texto = "(86) 99918-3229";
+const texto = "📍 Av. João Bandeira Monte, Nº 637 – Piripiri, PI";
 btnCopiar.addEventListener("click", async () => {
     try {
         await navigator.clipboard.writeText(texto);
         // Usa a API Clipboard para escrever o texto na área de transferência
-        alert("Número copiado para a área de transferência!");
+        alert("Copiado para a área de transferência.");
     }
     catch (err) {
         // Caso ocorra algum erro, mostramos no console
         console.error("Erro ao copiar: ", err);
     }
 });
-//# sourceMappingURL=copiar.js.map
