d151db4ec66acda21b7ac71bcc0435d6b194ac00
M.Vihtória <mariavitoriasilvaaraujo326@gmail.com>
2025-08-15 12:11:05 -0300

    script

--- script/copiar.js
+++ script/copiar.js
@@ -1,17 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const btnCopiar = document.getElementById("btnCopiar");
 // Texto que queremos copiar para a área de transferência
 const texto = "(86) 99918-3229";
+
 btnCopiar.addEventListener("click", async () => {
     try {
         await navigator.clipboard.writeText(texto);
         // Usa a API Clipboard para escrever o texto na área de transferência
         alert("Número copiado para a área de transferência!");
-    }
-    catch (err) {
+    } catch (err) {
         // Caso ocorra algum erro, mostramos no console
         console.error("Erro ao copiar: ", err);
     }
 });
-//# sourceMappingURL=copiar.js.map
