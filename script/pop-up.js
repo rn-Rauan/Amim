6f1d29ab991ec6bdcaf44b826089756fef2991be
M.Vihtória <mariavitoriasilvaaraujo326@gmail.com>
2025-08-15 02:34:48 -0300

    js

---
+++ script/pop-up.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const profileBtn = document.querySelector('.profile_btn');
+const dropdown = document.querySelector('.dropdown_wrapper');
+profileBtn.addEventListener('click', () => {
+    dropdown.classList.remove('none');
+    dropdown.classList.toggle('hide');
+});
+document.addEventListener('click', (event) => {
+    const isClickInsideDropdown = dropdown.contains(event.target);
+    const isProfileClicked = profileBtn.contains(event.target);
+    if (!isClickInsideDropdown && !isProfileClicked) {
+        dropdown.classList.add('hide');
+    }
+});
+//# sourceMappingURL=pop-up.js.map
